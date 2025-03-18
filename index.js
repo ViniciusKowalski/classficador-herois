478ad3aaa4ee7b454fb97406475d521c532acdf8
Vinicius Kowalski <vinicius.kowalski@animati.com.br>
2025-03-18 20:46:49 -0300

    Adciona comentário no código

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-/*
-Desafio Classificador de nível de Herói
-
-**O Que deve ser utilizado**
-
-- Variáveis
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-
-## Objetivo
-
-Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-
-Se XP for menor do que 1.000 = Ferro
-Se XP for entre 1.001 e 2.000 = Bronze
-Se XP for entre 2.001 e 5.000 = Prata
-Se XP for entre 5.001 e 7.000 = Ouro
-Se XP for entre 7.001 e 8.000 = Platina
-Se XP for entre 8.001 e 9.000 = Ascendente
-Se XP for entre 9.001 e 10.000= Imortal
-Se XP for maior ou igual a 10.001 = Radiante
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói de nome **{nome}** está no nível de **{nivel}**"
-*/
-
 //Entrada
 let heroi = "Homen de Ferro"
 let XP =  4500
-
 let nivel;
 
+//Verifica o nível do XP
 if (XP <= 1000){
     nivel="Ferro";
 }
@@ -57,7 +28,7 @@ else if (XP >= 9001 && XP <= 10000){
 else{ 
     nivel = "Radiente"
 }
-
+// saída 
 switch(nivel){
     case "Ferro":
         console.log("O Herói de nome " + heroi + " está no nível de " + nivel);
